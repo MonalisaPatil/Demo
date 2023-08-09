45f454d970a83f098cfde5e45166fd9a886f82e1
Monalisa Patil <90000984+MonalisaPatil@users.noreply.github.com>
2023-08-09 18:16:19 -0500

    [Jira-3]- added a Finding ASCII code of a number

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -2,44 +2,55 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String args[]) {
-        Add sum= new Add();
-        int result1 = sum.Addition(23,45);
-        System.out.println(result1);
+//        Add sum= new Add();
+//        int result1 = sum.Addition(23,45);
+//        System.out.println(result1);
+//
+//        Subtraction subtraction= new Subtraction();
+//        int result2 = subtraction.sub(43,67);
+//        System.out.println(result2);
+//
+//
+//        Multiply multiply= new Multiply();
+//        int result3 = multiply.multiplication(65, 78);
+//        System.out.println(result3);
+//
+//        Shadi shadi= new Shadi();
+//        System.out.println(shadi.kharcha(500, 1200));
+//
+//        // Area of Circle :---Static method program
+//        Scanner s= new Scanner(System.in);
+//        System.out.println(" Enter the radius");
+//        long r = s.nextLong();
+//        double rt = (double) r;
+//        double AOC = (22*rt*rt)/7;
+//        System.out.println(AOC);
+//
+//        //AOC ---using method
+//
+//
+//        //AOC ---inheritance method
+//
+//
+//        //AOC --- using interface
+//
+//        //AOC ---
+//
+//
+//        // Swap two numbers
+//        Swap sw= new Swap();
+//        sw.swap(56, 67);
+//
 
-        Subtraction subtraction= new Subtraction();
-        int result2 = subtraction.sub(43,67);
-        System.out.println(result2);
+        // Finding ASCII code of a number
+        Scanner number= new Scanner(System.in);
+        System.out.println("Enter the number:");
+        String v = number.next();
+        char x = v.charAt(0);
+        int t = (int)x;
+        System.out.println(t);
 
 
-        Multiply multiply= new Multiply();
-        int result3 = multiply.multiplication(65, 78);
-        System.out.println(result3);
-
-        Shadi shadi= new Shadi();
-        System.out.println(shadi.kharcha(500, 1200));
-
-        // Area of Circle :---Static method program
-        Scanner s= new Scanner(System.in);
-        System.out.println(" Enter the radius");
-        long r = s.nextLong();
-        double rt = (double) r;
-        double AOC = (22*rt*rt)/7;
-        System.out.println(AOC);
-
-        //AOC ---using method
-
-
-        //AOC ---inheritance method
-
-
-        //AOC --- using interface
-
-        //AOC ---
-
-
-        // Swap two numbers
-        Swap sw= new Swap();
-        sw.swap(56, 67);
    }
 }
 
