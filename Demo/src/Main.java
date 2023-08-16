047f9ccb0b54b0815a072909d99ad58933f46388
Monalisa Patil <90000984+MonalisaPatil@users.noreply.github.com>
2023-08-16 14:38:47 -0500

    added method to create array

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -1,12 +1,21 @@
 
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Hello World to feature branch array...");
+        char array1[] = new char[]{'a', 'b', 'c', 'd'};
+        char array2[] = new char[array1.length];
+        for (int c = 0; c < array1.length; c++) {
+            array2[c] = array1[c];
+        }
+        System.out.println("Element of Original array:");
+        for (int c = 0; c < array1.length; c++) {
+            System.out.println(array1[c]);
+        }
+
+        System.out.println("Element of duplicate array:");
+        for (int c = 0; c < array2.length; c++) {
+            System.out.println(array2[c]);
+        }
+
+
     }
 }
-
-
-
-
-
-
