7f01f52dce8b64cabf769a66378ad75b337bd4ad
Monalisa Patil <90000984+MonalisaPatil@users.noreply.github.com>
2023-08-16 12:09:46 -0500

    added method to create map

--- Demo/src/Main.java
+++ Demo/src/Main.java
@@ -1,70 +1,67 @@
 import java.util.*;
 
 public class Main {
+
+    //method 1
     public static void main(String args[]) {
-   // creating entry
-
-        Map<Integer, String> classes = new HashMap<>();
-        classes.put(1, "name1");
-        classes.put(2,"name2");
-        classes.put(3, "name3");
-
-        for (Map.Entry<Integer, String> i: classes.entrySet()){
-            System.out.println("Roll number: "+ i.getKey() + " is " + i.getValue());
-
-        }
-
-        Map<String, Integer> map= new HashMap<>();
-        map.put("shubham", 1);
-        map.put("monfgfa", 4);
-        map.put("tuvjlsi", 4);
-        map.put("vhhvh", 4);
-        map.put("mummy", 4);
-        map.put("papa",6);
-        map.put("shubham", 7);
-        map.put("tulsi", 8);
-        map.put("mummy", 9);
-        map.put("tulsi", 3);
 
 
-        Set<String> jisKoLoopKarnaHai = map.keySet();
-         for (String individualDataType: jisKoLoopKarnaHai){
-             System.out.println(individualDataType);
-         }
 
-         Map<Integer,Integer> CountMap = new HashMap<>();
-         Collection<Integer> collection = map.values();
-         Iterator<Integer> iterator = collection.iterator();
 
-//        for (Integer i: collection){
-//            if (CountMap.containsValue(collection.get(i))){
-//                CountMap.replace(collection.get(i),CountMap.get(collection.get(i))+1);
-//            }
-//            else {
-//                CountMap.put(collection.get(i),i);
-//            }
+
+
+
+    }
+//        boolean result = isPalindrome(101);
+//        System.out.println(result);
+//        Roman roman = new Roman();
 //
-//            System.out.println(CountMap);
+//        Roman r= new Roman();
+//        int y= r.romanToInt()
+//       int ru=  roman.romanToInt("");
+//        int ru2=  roman.romanToInt2(3,"tyu",'k');
+//    //    }
+//        int RomanToInt = romanToInt("V");
 //
+//        int n1 = 0, n2 = 1, n3, i, count=10;
+//        System.out.println(n1 + ", " + n2);
+//
+//        for( i=0; i<count;++i){
+//            n3 = n1+n2;
+//            System.out.println(" "+ n3);
+//            n1=n2;
+//            n2=n3;
 //        }
 
-        while (iterator.hasNext()){
-            if (CountMap.containsValue(iterator.next())){
-                CountMap.replace(iterator.next(),CountMap.get(iterator.next())+1);
-            }
-            else {
-                CountMap.put(iterator.next(),1);
-            }
-            System.out.println(CountMap);
-        }
+//    }
 
+//
+//    }
 
-        }
+//    public static boolean isPalindrome(int x) {
+//        if (x < 0) {
+//            return false;
+//        }
+        //approach-1
+//        int reversed = 0;
+//        int temp = x;
 
+//        while (temp != 0) {
+//            int digit = (temp % 10);
+//            reversed = reversed * 10 + digit;
+//            temp= temp/10;
+//        }
+//        return (reversed == x);
 
-
-        }
-
+        //approach-2
+//        String inputString= String.valueOf(x);
+//        String reversedString="";
+//        for (int i=inputString.length()-1; i>=0;i--){
+//            reversedString=reversedString+ inputString.charAt(i);
+//        }
+//
+//        return inputString.equalsIgnoreCase(reversedString);
+//    }
 
 
 
---
+++ Demo/src/Roman.java
@@ -0,0 +1,59 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Roman {
+
+    int a;
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+
+        return ans;
+    }
+
+    public int romanToInt2(int it,String s, char c) {
+        Map<Character, Integer> m = new HashMap<>();
+
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                // "XV".charAt(index value-0)=====X
+                // "XV".charAt(index value-1)=====V
+                char key = s.charAt(i);
+                ans += m.get(key);
+            }
+        }
+
+        return ans;
+    }
+
+}
